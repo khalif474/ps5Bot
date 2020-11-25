2bef1347f94e7e148907a5d42d56692237f2bdda
Abdulahi khalif <abdulahikhalif474@gmail.com>
2020-11-26 00:17:24 +1100

    first commit

---
+++ emailDeliver.py
@@ -0,0 +1,20 @@
+import smtplib
+import ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "botjeff506@gmail.com"  # Enter your address
+receiver_email = "abdurahmankhalif474@gmail.com"  # Enter receiver address
+password = "yeet1234"
+message = """\
+Subject: PS5 IS OUT !!!
+
+Website changes detected from ps5: Get it Now Now Hurry Hurry Last Chance"""
+
+print("now")
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    for i in range(1):
+        server.sendmail(sender_email, receiver_email, message)
---
+++ ps5BotChecker.py
@@ -0,0 +1,91 @@
+import requests
+import time
+import smtplib
+from email.message import EmailMessage
+import hashlib
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import ssl
+
+
+# ----------------------------- Functions
+
+# ---------------------------------
+def findArticles(currentArticles):
+    articles_list = []
+    articles = currentArticles.find_all('article')
+    for article in articles:
+        articles_list.append(article.find('div', {"class": "preview-mini-wrap clearfix"}).find('div', {
+            "class": "meta"}).find('div', {"class": "title-wrap"}).find('h3', {"class": "title"}).text)
+    return articles_list
+
+
+# Mail Setting =============================================================================
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "botjeff506@gmail.com"  # Enter your address
+receiver_email = "abdurahmankhalif474@gmail.com"  # Enter receiver address
+password = "yeet1234"
+message = """\
+Subject: Press AU New News !!!
+
+Press AU New NEWS CHECK IT OUT !!!"""
+
+print("1")
+# ============================================================================================
+
+url = 'https://press-start.com.au/'
+req = Request(
+    url,
+    data=None,
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+    }
+)
+response = urlopen(req).read()
+soup = BeautifulSoup(response, 'html.parser')
+currentArticles = soup.find(
+    'div', {"class": "block block-1 clearfix preview-review-bot"})
+final_currentArticles = findArticles(currentArticles)
+
+
+while True:
+
+    try:
+        time.sleep(60)
+        response = urlopen(req).read()
+        soup = BeautifulSoup(response, 'html.parser')
+        currentArticles = soup.find(
+            'div', {"class": "block block-1 clearfix preview-review-bot"})
+        newArticle = findArticles(currentArticles)
+        if newArticle == final_currentArticles:
+            print("No changes to site - "+url)
+            continue
+
+        else:
+
+            print("Change Detected url - "+url)
+            context = ssl.create_default_context()
+            # Send message
+            message = """\
+            Subject: Press AU New News !!!
+
+            Press AU New NEWS CHECK IT OUT  With new Article """+newArticle[0]+"""!!!"""
+            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                server.login(sender_email, password)
+                for i in range(20):
+                    server.sendmail(sender_email, receiver_email, message)
+
+            # If article new is found we need to reset current hash
+            response = urlopen(req).read()
+            soup = BeautifulSoup(response, 'html.parser')
+            currentArticles = soup.find(
+                'div', {"class": "block block-1 clearfix preview-review-bot"})
+            final_currentArticles = findArticles(currentArticles)
+
+            continue
+
+    except Exception as e:
+        print("Server Error")
+        print(e)
